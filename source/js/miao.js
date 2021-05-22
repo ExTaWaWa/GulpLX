e3b4b4a035b683f8b3e7fa3d23ed1e4ed345502c
ExTaWaWa <nienchan@gmail.com>
2021-05-22 19:39:25 +0800

    upload heroku

---
+++ source/js/miao.js
@@ -0,0 +1,31 @@
+// setInterval(function() { console.log("hello"); }, 1000);
+var http = require("http");
+
+
+//  node.js 載入模組
+
+
+http.createServer(function(request, response) {
+  console.log(request.url);
+  if(request.url == '/'){
+// 路由
+    // 資料庫
+    console.log("接收到網頁請求！");
+    response.writeHead(200, {"Content-Type": "text/HTML"});
+    response.write("<h1>index</h1>");
+    response.end();
+  }else if(request.url == '/search'){
+     console.log("接收到網頁請求！");
+    response.writeHead(200, {"Content-Type": "text/HTML"});
+    response.write("<h1>search</h1>");
+    response.end();
+  }else{
+    response.writeHead(200, {"Content-Type": "text/HTML"});
+    response.write("<head><meta charset='UTF-8'></head>");
+    response.write("<h1>好興奮</h1>");
+    response.end();
+  }
+  
+}).listen(process.env.PORT ||3000);
+console.log("Server已開啟port: 3000.");
+
